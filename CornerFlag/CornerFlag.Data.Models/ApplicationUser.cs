5735fe5b4d013ad50f0feba881ca7f03be6f38f5
theexar <easlavov@gmail.com>
2014-11-08 10:20:34 +0200

    Enabled migrations. ApplicationUser now implements IAuditInfo and IDeletableEntity.

--- CornerFlag/CornerFlag.Data.Models/ApplicationUser.cs
+++ CornerFlag/CornerFlag.Data.Models/ApplicationUser.cs
@@ -5,10 +5,17 @@
 
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using CornerFlag.Data.Contracts.Models;
+    using System;
 
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
     {
+        public ApplicationUser()
+        {
+            this.CreatedOn = DateTime.Now;
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,5 +23,15 @@
             // Add custom user claims here
             return userIdentity;
         }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
     }
 }
