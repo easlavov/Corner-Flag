3ee107c59b94141a84b16dda6a4f6dd367991ce3
theexar <easlavov@gmail.com>
2014-11-08 09:43:57 +0200

    Added classes to handle safe deletion of entities from the database.

---
+++ CornerFlag/CornerFlag.Data.Contracts/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CornerFlag.Data.Contracts.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ CornerFlag/CornerFlag.Data.Contracts/Models/IOrderable.cs
@@ -0,0 +1,7 @@
+﻿namespace CornerFlag.Data.Contracts.Models
+{
+    public interface IOrderable
+    {
+        int OrderBy { get; set; }
+    }
+}
