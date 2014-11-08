6fde6fa5fdd049813ab8141982721e8a0de85a67
theexar <easlavov@gmail.com>
2014-11-08 18:55:35 +0200

    Created BaseController.cs Modified Constants.cs

---
+++ CornerFlag/CornerFlag.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using CornerFlag.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XMLSoccerCOM;
+
+namespace CornerFlag.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            this.SoccerData = new Requester(Constants.XmlSoccerApiKey);
+        }
+
+        protected Requester SoccerData { get; set; }
+    }
+}
--- CornerFlag/CornerFlag.Web/Controllers/HomeController.cs
+++ CornerFlag/CornerFlag.Web/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
-﻿using CornerFlag.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using XMLSoccerCOM;
 
 namespace CornerFlag.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
+            var team = this.SoccerData.GetTeam("Aberdeen");
+            return View(team);
         }
 
         public ActionResult About()
