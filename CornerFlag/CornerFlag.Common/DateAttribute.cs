4b97542b3b14d083e54ca2d1e9d3f93d44f8b49e
theexar <easlavov@gmail.com>
2014-11-13 10:19:55 +0200

    Added DateAttribute for range of dates data annotations

---
+++ CornerFlag/CornerFlag.Common/DateAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace CornerFlag.Common
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class DateAttribute : RangeAttribute
+    {
+        public DateAttribute(int yearsFromTodayMinimum, int yearsFromTodayMaximum)
+            : base(typeof(DateTime),
+            DateTime.Now.AddYears(yearsFromTodayMinimum).ToShortDateString(),
+            DateTime.Now.AddYears(yearsFromTodayMaximum).ToShortDateString()) 
+        {             
+        }
+    }
+}
