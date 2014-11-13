c4b900aa98f5b0f106e20fdf0abf82377e347e6b
theexar <easlavov@gmail.com>
2014-11-13 16:26:43 +0200

    Added seeding methods for Countries, Clubs and Players.

--- CornerFlag/CornerFlag.Data/Migrations/Configuration.cs
+++ CornerFlag/CornerFlag.Data/Migrations/Configuration.cs
@@ -6,9 +6,17 @@ namespace CornerFlag.Data.Migrations
     using System.Linq;
 
     using CornerFlag.Data;
+    using CornerFlag.Data.Models.Entities;
+    using CornerFlag.Data.Models.People;
+    using CornerFlag.Common;
 
     internal sealed class Configuration : DbMigrationsConfiguration<CornerFlagDbContext>
     {
+        private const int PLAYERS_COUNT = 160;
+        private const int CLUBS_COUNT = 16;
+        private const int COMPETITIONS_COUNT = 4;
+        private const int COUNTRIES_COUNT = 2;
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
@@ -17,6 +25,13 @@ namespace CornerFlag.Data.Migrations
 
         protected override void Seed(CornerFlagDbContext context)
         {
+            context.Configuration.AutoDetectChangesEnabled = false;
+            this.SeedCountries(context);
+            this.SeedPlayers(context);
+            this.SeedClubs(context);
+            //this.SeedCompetitions(context);
+
+            context.Configuration.AutoDetectChangesEnabled = true;
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -29,6 +44,74 @@ namespace CornerFlag.Data.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+
+        private void SeedCompetitions(CornerFlagDbContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SeedClubs(CornerFlagDbContext context)
+        {
+            var generator = new RandomDataGenerator();
+            var from = DateTime.Now.AddYears(-40);
+            var to = DateTime.Now.AddYears(-10);
+            for (int i = 0; i < CLUBS_COUNT; i++)
+            {
+                var club = new Club();
+                club.Name = generator.GetString(2, 20);
+                club.DateFounded = generator.GetRandomDate(from, to);
+                var country = context.Countries.First(c => c.Clubs.Count < (COUNTRIES_COUNT * COMPETITIONS_COUNT));
+                club.Country = country;
+                var players = context.Players.Where(p => p.Club == null).Take(10);
+                foreach (var player in players)
+                {
+                    club.Team.Add(player);
+                }
+
+                country.Clubs.Add(club);
+                context.Clubs.AddOrUpdate(club);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCountries(CornerFlagDbContext context)
+        {
+            int countriesCount = 10;
+            var generator = new RandomDataGenerator();
+            for (int i = 0; i < countriesCount; i++)
+            {
+                var country = new Country();
+                country.Name = generator.GetString(2, 20);
+                context.Countries.AddOrUpdate(country);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedPlayers(CornerFlagDbContext context)
+        {
+            int playersCount = PLAYERS_COUNT;
+            var countries = context.Countries;
+            var generator = new RandomDataGenerator();
+            var from = DateTime.Now.AddYears(-40);
+            var to = DateTime.Now.AddYears(-10);
+            for (int i = 0; i < playersCount; i++)
+            {
+                var player = new Player();
+                player.BirthDate = generator.GetRandomDate(from, to);
+                player.FirstName = generator.GetString(2, 20);
+                player.LastName = generator.GetString(2, 20);
+                var countryId = generator.GetInt(1, 10);
+                player.Country = countries.First(c => c.Id == countryId);
+                context.Players.AddOrUpdate(player);
+                if (i % 100 == 0)
+                {
+                    context.SaveChanges();
+                }
+            }
+
+            context.SaveChanges();
         }
     }
 }
