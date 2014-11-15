6f978c11cc40d09a2c688a62f2df920ce631903d
theexar <easlavov@gmail.com>
2014-11-15 17:33:09 +0200

    Set up automapper

--- CornerFlag/CornerFlag.Web/Global.asax.cs
+++ CornerFlag/CornerFlag.Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CornerFlag.Web.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -19,6 +21,9 @@ namespace CornerFlag.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var automapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            automapperConfig.Execute();
         }
     }
 }
