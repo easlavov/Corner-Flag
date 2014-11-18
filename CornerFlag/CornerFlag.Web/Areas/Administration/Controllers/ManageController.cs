638f4c1258c9282b6b89ee9f386e4dbc8f82ca8d
theexar <easlavov@gmail.com>
2014-11-18 13:09:10 +0200

    Kendo grid for managing countries

---
+++ CornerFlag/CornerFlag.Web/Areas/Administration/Controllers/ManageController.cs
@@ -0,0 +1,65 @@
+﻿namespace CornerFlag.Web.Areas.Administration.Controllers
+{
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.Extensions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using CornerFlag.Web.Controllers;
+    using CornerFlag.Web.Areas.Administration.ViewModels;
+    using AutoMapper.QueryableExtensions;
+    using CornerFlag.Data;
+
+    public class ManageController : BaseController
+    {
+        public ManageController(ICornerFlagData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Countries()
+        {
+            return View();
+        }
+
+        public ActionResult Countries_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<CountryViewModel> products = this.data.Countries.AllWithDeleted().Project().To<CountryViewModel>();
+            DataSourceResult result = products.ToDataSourceResult(request);
+            return Json(result);
+        }
+
+        public ActionResult Edit_Country([DataSourceRequest]DataSourceRequest request, CountryViewModel input)
+        {
+            try
+            {
+                var postToUpdate = this.data.Countries.GetById(input.Id);
+                postToUpdate.Name = input.Name;
+
+                this.data.Countries.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+
+            return Json(new[] { input }.ToDataSourceResult(request, ModelState));
+        }
+
+        public ActionResult Destroy_Country([DataSourceRequest]DataSourceRequest request, CountryViewModel input)
+        {
+            try
+            {
+                var postId = this.data.Countries.GetById(input.Id);
+                this.data.Countries.Delete(postId);
+                this.data.Countries.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+
+            return Json(new[] { input }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
---
+++ CornerFlag/CornerFlag.Web/Areas/Administration/ViewModels/CountryViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CornerFlag.Web.Areas.Administration.ViewModels
+{
+    using CornerFlag.Data.Models.Entities;
+    using CornerFlag.Web.Infrastructure.Mapping;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class CountryViewModel : IMapFrom<Country>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
